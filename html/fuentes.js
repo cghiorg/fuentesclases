667084ba475d954cf1c24fd207bea443bd8d5171
cghiorg <cghiorg@gmail.com>
2025-05-02 20:04:35 -0300

    Clase 5

---
+++ html/fuentes.js
@@ -0,0 +1,7 @@
+function validarFormulario() {
+    let nombre = document.getElementById('nombre').value;
+    if (nombre.trim() === '') {
+      alert('Por favor, ingresá tu nombre.');
+      return false;
+    }
+  }
